28506bb3751ef1471e1f2e0ceb1f9f1b0f9bc391
brothermin <brothermin@kaoni.com>
2025-04-28 17:28:52 +0900

    a태그로 동일페이지 새로고침

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,16 @@
 /** 공통 헤더 컴포넌트 */
-import Link from "next/link";
 
 export default function Header() {
     return (
         <header>
-            <h1>MBTI 웹사이트</h1>
-            <nav>
-                <Link href="/">홈</Link>
-            </nav>
-            <br/>
+            <h1>
+                <nav>
+                    {/* 동일위치에서 Link는 작동하지 않기 때문에 a태그 사용*/}
+                    <a href="/" style={{textDecoration: 'none'}}>
+                        MBTI 웹사이트
+                    </a>
+                </nav>
+            </h1>
         </header>
     );
 }
