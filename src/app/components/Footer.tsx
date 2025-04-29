6f24455a65af61d48be2f64ca554fde0c96f824e
brothermin <brothermin@kaoni.com>
2025-04-29 10:38:39 +0900

    style : 푸터 단순 텍스트 변경

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p>© 2025 MBTI 웹사이트</p>
+            <p>이 웹사이트는 MBTI®와 무관하며, 비공식 테스트로 결과는 단순 재미와 참고용입니다.😊</p>
         </footer>
     )
 }
