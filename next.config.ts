ddf2fe18e3d9ba1f79f9e2d001e6e7e3d5975938
brothermin <brothermin@kaoni.com>
2025-05-02 14:54:07 +0900

    fix : 환경설정에 domains 추가(이미지 안보이는 문제)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'standalone',
+    images: {
+        domains: ['175.45.203.119', 'localhost']
+    },
 };
 
 export default nextConfig;
