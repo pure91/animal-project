67bda54a7dd3cffd35a4fa91d851c221b4900631
brothermin <brothermin@kaoni.com>
2025-05-07 15:09:14 +0900

    feat : 웹사이트, 모바일환경 화면을 위한 viewport 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,19 @@ import "./globals.css";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import {ReactNode} from "react";
-
+import Head from "next/head";
 
 export default function RootLayout({children}: { children: ReactNode }) {
     return (
         <html>
-        <body>
-            <Header/>
-            <main>{children}</main>
-            <Footer/>
-        </body>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            </Head>
+            <body>
+                <Header/>
+                <main>{children}</main>
+                <Footer/>
+            </body>
         </html>
     );
 }
