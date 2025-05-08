9907fe89f4e7b10be3a79f1611d3203c1a66341f
brothermin <brothermin@kaoni.com>
2025-05-08 09:56:10 +0900

    refactor : 외부 도메인 이미지 로드 기능 최신 버전 변경(domains -> remotePatterns)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
     output: 'standalone',
     images: {
-        domains: ['175.45.203.119', 'localhost']
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '175.45.203.119',
+                port: '3000',
+                pathname: '/images/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+                pathname: '/images/**',
+            },
+        ],
     },
 };
 
