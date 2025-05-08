6a3659b07eaa70c524bc37e19f9270d7b4b53f4c
brothermin <brothermin@kaoni.com>
2025-05-08 12:44:27 +0900

    chore : KaKao API type정의 any 시 배포 문제 룰 무시 주석 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,23 +5,24 @@ import {ReactNode} from "react";
 import Script from "next/script";
 
 declare global {
-  interface Window {
-    Kakao: any;
-  }
+    interface Window {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        Kakao: any;
+    }
 }
 
 export default function RootLayout({children}: { children: ReactNode }) {
     return (
         <html>
-            <body>
-                <Script
-                    src="https://developers.kakao.com/sdk/js/kakao.js"
-                    strategy="beforeInteractive"
-                />
-                <Header/>
-                    <main>{children}</main>
-                <Footer/>
-            </body>
+        <body>
+        <Script
+            src="https://developers.kakao.com/sdk/js/kakao.js"
+            strategy="beforeInteractive"
+        />
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+        </body>
         </html>
     );
 }
