6abb7a9738f6823b53d100a907a2e3c4a411611e
brothermin <brothermin@kaoni.com>
2025-05-12 13:17:34 +0900

    docs : 도메인 등록으로인한 이미지 패턴 변경

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,15 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: '175.45.203.119',
-                port: '3000',
+                protocol: 'https',
+                hostname: 'zootypes.com',
+                port: '', // 443은 생략 가능
                 pathname: '/images/**',
             },
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '3000',
+                protocol: 'https',
+                hostname: 'www.zootypes.com',
+                port: '',
                 pathname: '/images/**',
             },
         ],
