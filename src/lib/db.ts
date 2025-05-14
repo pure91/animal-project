90020c7dd614ada2c22fdc29f3c4266886378637
brothermin <brothermin@kaoni.com>
2025-05-14 14:46:48 +0900

    refactor : db정보는 로컬에서, 서버는 git에서

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 // MariaDb 연결
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
