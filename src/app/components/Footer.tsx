fe3b790c40b81566fea38d63202bc2b8e274fb17
pure91 <kimgudals91@naver.com>
2025-05-18 18:20:27 +0900

    docs : 푸터 text 변경

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p>본 테스트는 MBTI®와 유사한 성격 분류 모델을 참고하였으며, 공식 MBTI® 검사와는 무관한 비공식 테스트입니다. 단순 재미로 즐겨주세요😊</p>
+            <p>해당 테스트는 MBTI® 성격유형 이론을 참고하여 제작된 자체 콘텐츠로 공식 MBTI® 검사와는 무관합니다. 단순 재미로 즐겨주세요😊</p>
         </footer>
     )
 }
