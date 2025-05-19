dac39ebafcee1ec046173336829547c652e2ecb6
brothermin <brothermin@kaoni.com>
2025-05-19 13:39:16 +0900

    feat : 애드핏 광고 등록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,15 +16,15 @@ export default function RootLayout({children}: { children: ReactNode }) {
         <html>
         <head>
             {/* Google AdSense */}
-            <script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3666035347659822"
-                crossOrigin="anonymous"
-            />
-            <meta
-                name="google-adsense-account"
-                content="ca-pub-3666035347659822"
-            />
+            {/*<script*/}
+            {/*    async*/}
+            {/*    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3666035347659822"*/}
+            {/*    crossOrigin="anonymous"*/}
+            {/*/>*/}
+            {/*<meta*/}
+            {/*    name="google-adsense-account"*/}
+            {/*    content="ca-pub-3666035347659822"*/}
+            {/*/>*/}
         </head>
         <body>
         {/* Kakao Share */}
@@ -34,6 +34,26 @@ export default function RootLayout({children}: { children: ReactNode }) {
         />
         <Header/>
         <main>{children}</main>
+        {/* 애드핏 광고 */}
+        {/* PC용 세로로 긴 직사각형 */}
+        <ins className="kakao_ad_area" style={{display: "none"}}
+             data-ad-unit="DAN-7NrcKlwYN0GSSzow"
+             data-ad-width="160"
+             data-ad-height="600"
+        ></ins>
+        {/* 모바일용 얇은 띠배너형 */}
+        <ins className="kakao_ad_area" style={{display: "none"}}
+             data-ad-unit="DAN-Z3dBCJFSTNoWEHvK"
+             data-ad-width="320"
+             data-ad-height="50"
+        ></ins>
+        {/* PC/M 겸용 정사각에 가까운 직사각형 */}
+        <ins className="kakao_ad_area" style={{display: "none"}}
+             data-ad-unit="DAN-HJOimM1VFybaBUeU"
+             data-ad-width="300"
+             data-ad-height="250"
+        ></ins>
+        <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
         <Footer/>
         </body>
         </html>
