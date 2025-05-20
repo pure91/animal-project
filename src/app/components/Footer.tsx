008927e672ae70963143efc78e24b7f1f7efcfed
brothermin <brothermin@kaoni.com>
2025-05-20 10:11:48 +0900

    docs : 푸터 저작권 표시

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <p>해당 테스트는 MBTI® 성격유형 이론을 참고하여 제작된 자체 콘텐츠로 공식 MBTI® 검사와는 무관합니다. 단순 재미로 즐겨주세요😊</p>
+            <p>Copyright ⓒ 2025 Zootypes. All rights reserved.</p>
         </footer>
     )
 }
