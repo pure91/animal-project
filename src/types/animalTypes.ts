5dfed7d17ecf7e62e5fbad19f15fc9c0652d9a83
brothermin <brothermin@kaoni.com>
2025-05-21 17:16:24 +0900

    refactor : types 정의 분리

---
+++ src/types/animalTypes.ts
@@ -0,0 +1,20 @@
+/** 타입 정의 */
+
+// traits 키 배열
+export type TraitKeys = "W" | "X" | "A" | "I" | "F" | "T" | "S" | "U";
+
+// 유형별 레벨 정의
+export type LevelKeys = "1" | "2" | "3" | "4";
+
+// 하위 타입
+export type Subtype = {
+    name: string;
+    description: string;
+    traits: Record<TraitKeys, number>;
+    characteristics: string[];
+};
+
+// 동물 유형
+export type AnimalData = {
+    types: Record<LevelKeys, Subtype[]>;
+};
