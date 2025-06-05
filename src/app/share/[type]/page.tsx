57fe02f7d002ab2fa3eec0b6ba99799f9da3f5ae
brothermin <brothermin@kaoni.com>
2025-06-05 17:41:22 +0900

    fix : redirect는 서버 컴포넌트내에서만 동작(async는 선택) -> use client 제거, params 타입 넓게 조정

--- src/app/share/[type]/page.tsx
+++ src/app/share/[type]/page.tsx
@@ -1,13 +1,10 @@
-"use client"
-
 import {redirect} from 'next/navigation';
 
-type Props = {
-  params: {
-    type: string;
-  };
-};
-
-export default function ShareRedirectPage({ params }: Props) {
-  redirect(`/result?type=${params.type}`);
+export default async function ShareRedirectPage({
+  params,
+}: {
+  params: { type: string | string[] };
+}) {
+  const type = Array.isArray(params.type) ? params.type[0] : params.type;
+  redirect(`/result?type=${type}`);
 }
