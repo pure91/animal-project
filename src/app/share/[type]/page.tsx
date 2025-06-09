03f4dad1da912fad8d080f033f3451ed95cb7408
brothermin <brothermin@kaoni.com>
2025-06-09 10:01:26 +0900

    fix : 15.1버전부터 params 타입이 Promise로 변경되어서 타입에러 났던것.. 다시 하나로 합치고 서버 컴포넌트로 바꿈

--- src/app/share/[type]/page.tsx
+++ src/app/share/[type]/page.tsx
@@ -1,15 +1,57 @@
-"use client";
+import { redirect } from "next/navigation";
+import rawAnimalTypes from "@/app/data/animalTypes.json";
+import type {AnimalData} from "@/types/animalTypes";
+import type {Metadata} from "next";
+import {getCharacterProfile} from "@/utils/animalUtils";
 
-import {useEffect} from "react";
-import {useRouter} from "next/navigation";
+const animalTypes = rawAnimalTypes as Record<string, AnimalData>;
 
-/** redirect용 클라이언트 컴포넌트 */
-export default function ShareRedirectPage({ params }: { params: { type: string } }) {
-  const router = useRouter();
+// 타입별 동물 이미지 매핑
+const animalImages: Record<string, string> = {
+    IRES: "/images/hedgehog.png",
+    IRCS: "/images/turtle.png",
+    IDES: "/images/cat.png",
+    IDCS: "/images/penguin.png",
+    IREA: "/images/rabbit.png",
+    IRCA: "/images/badger.png",
+    IDEA: "/images/fox.png",
+    IDCA: "/images/weasel.png",
+    ORES: "/images/dog.png",
+    ORCS: "/images/wolf.png",
+    ODES: "/images/lion.png",
+    ODCS: "/images/elephant.png",
+    OREA: "/images/dolphin.png",
+    ORCA: "/images/shark.png",
+    ODEA: "/images/squirrel.png",
+    ODCA: "/images/octopus.png",
+    HUMAN: "/images/human.png",
+};
 
-  useEffect(() => {
-    router.push(`/result?type=${params.type}`);
-  }, [params.type, router]);
+// 하나 이상의 메타데이터 필드를 포함하는 Metadata 객체를 반환(동적 페이지 메타데이터 생성 방법 -> use client 쓰면 안됨)
+export async function generateMetadata({params}: { params: Promise<{ type: string }> }): Promise<Metadata> {
+    const { type } = await params; // 15.1버전 이후부터 params를 Promise로 받아야하고 그 실제값도 꺼내줘야함
+    const animalData = animalTypes[type];
+    const imageUrl = `https://zootypes.com${animalImages[type] ?? "/images/animalAll.png"}`;
+    const characterProfile = animalData?.types
+        ? getCharacterProfile(
+            {I: 0, O: 0, R: 0, D: 0, E: 0, C: 0, S: 0, A: 0},
+            animalData.types
+        )
+        : null;
 
-  return null;
+    return {
+        title: `나의 유형은 ${type}`,
+        description: `⭐${characterProfile?.name ?? "알 수 없음"}⭐ - ${characterProfile?.description ?? ""}`,
+        openGraph: {
+            title: `나의 유형은 ${type}`,
+            description: `⭐${characterProfile?.name ?? "알 수 없음"}⭐ - ${characterProfile?.description ?? ""}`,
+            images: [imageUrl],
+            url: `https://zootypes.com/share/${type}`,
+        },
+    };
+}
+
+export default async function Page({ params }: { params: Promise<{ type: string }> }) {
+  const { type } = await params;
+  redirect(`/result?type=${type}`);
 }
