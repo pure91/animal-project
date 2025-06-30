b3a215e8545ee5ec1ea430955839cbcab8231354
pure91 <kimgudals91@naver.com>
2025-06-30 12:16:50 +0900

    chore : 질문지 text 축약

--- src/app/data/questions.ts
+++ src/app/data/questions.ts
@@ -20,14 +20,14 @@ const questions: Question[] = [
             {text: "말 걸 생각조차 안함", score: {I: 5}},
             {text: "보통 먼저 나서진 않는다", score: {I: 3}},
             {text: "상황에 따라 다름", score: {I: 1, O: 1}},
-            {text: "어색하지만 먼저 말을 거는 편", score: {O: 3}},
-            {text: "주도적으로 분위기를 띄워본다", score: {O: 5}},
+            {text: "어색하지만 말을 거는 편", score: {O: 3}},
+            {text: "주도적으로 분위기를 띄운다", score: {O: 5}},
         ]
     },
     {
-        question: "약속 없이 쉬는 날 아침, 친구가 카톡으로 급 만남 제안!",
+        question: "쉬는 날 아침, 친구가 카톡으로 급 만남 제안!",
         options: [
-            {text: "카톡 겉으로 읽고 못 본 척 한다", score: {I: 5}},
+            {text: "겉으로 읽고 못 본 척 한다", score: {I: 5}},
             {text: "귀찮지만 고민해보고.. 답장은 해준다", score: {I: 3}},
             {text: "상황 봐서 결정", score: {I: 1, O: 1}},
             {text: "오? 오늘 할거 없었는데 나가볼까?", score: {O: 3}},
@@ -57,7 +57,7 @@ const questions: Question[] = [
     {
         question: "동네 마트에서 애매한 사이의 아는 사람 마주침",
         options: [
-            {text: "날 아직 못봤다면 빨리 다른 코너로 도망감", score: {I: 5}},
+            {text: "날 아직 못봤다면 다른 코너로 도망감", score: {I: 5}},
             {text: "먼저 아는척 안하면 그냥 모른 척함", score: {I: 3}},
             {text: "상황 봐서 인사하거나 말거나", score: {I: 1, O: 1}},
             {text: "어? 너 여기 왜 있어~? 먼저 말 건다", score: {O: 3}},
@@ -81,15 +81,15 @@ const questions: Question[] = [
             {text: "일단 안 받고 인터넷에 번호 검색해봄", score: {I: 3}},
             {text: "받진 않지만, 누구세요? 문자는 보냄", score: {I: 1, O: 1}},
             {text: "한 번 받아봄. 혹시 뭐 재밌는 일?", score: {O: 3}},
-            {text: "여보세요~ 누구세요~? 자연스러운 인싸 모드", score: {O: 5}},
+            {text: "여보세요~ 누구세요~? 인싸 모드 발동", score: {O: 5}},
         ]
     },
     {
-        question: "많은 사람이 모인 자리, 무대로 나오라고 지목당했을 때 반응은?",
+        question: "많은 사람이 모인 자리, 무대로 나오라고 했을 때",
         options: [
-            {text: "내 안의 영혼까지 도망감, 절대 안나감", score: {I: 5}},
-            {text: "어떻게든 계속 버텨보다가 할 수 없이 나감", score: {I: 3}},
-            {text: "그럭저럭 천천히 나가본다", score: {I: 1, O: 1}},
+            {text: "영혼까지 도망감, 절대 안나감", score: {I: 5}},
+            {text: "계속 버텨보다가 할 수 없으면 나감", score: {I: 3}},
+            {text: "고민하다가 천천히 나가본다", score: {I: 1, O: 1}},
             {text: "조금 떨리긴한데 설레는 마음으로 나간다", score: {O: 3}},
             {text: "기회다! 마이크 잡고 내 끼를 방출한다", score: {O: 5}},
         ]
@@ -107,13 +107,13 @@ const questions: Question[] = [
 
     // R vs D
     {
-        question: "고양이가 갑자기 당신에게 와서 뚫어지게 쳐다본다",
+        question: "고양이가 갑자기 당신을 뚫어지게 쳐다본다",
         options: [
             {text: "아우 귀여워 간식이라도 줘야 하나?", score: {R: 5}},
             {text: "같이 눈싸움 함", score: {R: 3}},
             {text: "뭐지? 왜 이러지?", score: {R: 1, D: 1}},
             {text: "내 안에 뭔가를 보고 있는건가…", score: {D: 3}},
-            {text: "고양이야, 혹시 내 과거를 본 거니? 라고 말을 건다", score: {D: 5}},
+            {text: "고양이야, 혹시 내 과거를 본 거니?", score: {D: 5}},
         ]
     },
     {
@@ -127,6 +127,16 @@ const questions: Question[] = [
         ]
     },
     {
+        question: "택시기사님이 한 마디도 안 하심",
+        options: [
+            {text: "조용해서 좋음", score: {R: 5}},
+            {text: "어색하긴 한데 괜찮음", score: {R: 3}},
+            {text: "말 걸까 말까 고민함", score: {R: 1, D: 1}},
+            {text: "나 혹시 뭔가 잘못한 거 아니야..?", score: {D: 3}},
+            {text: "지금 납치된 건 아닐까? 시나리오 돌림", score: {D: 5}},
+        ]
+    },
+    {
         question: "내일 비 온대요. 당신의 반응은?",
         options: [
             {text: "우산 챙겨야겠다!", score: {R: 5}},
@@ -134,16 +144,6 @@ const questions: Question[] = [
             {text: "비? 뭐 어쩔", score: {R: 1, D: 1}},
             {text: "비랑 관련된 플레이리스트 쫙 틀어야지", score: {D: 3}},
             {text: "뭔가 운명적인 일이 생길 것 같아", score: {D: 5}},
-        ]
-    },
-    {
-        question: "택시기사님이 한 마디도 안 하심",
-        options: [
-            {text: "조용해서 좋음", score: {R: 5}},
-            {text: "어색하긴 한데 괜찮음", score: {R: 3}},
-            {text: "말 걸까 말까 고민함", score: {R: 1, D: 1}},
-            {text: "나 혹시 뭔가 잘못한 거 아니야…?", score: {D: 3}},
-            {text: "지금 납치된 건 아닐까 하는 시나리오 돌림", score: {D: 5}},
         ]
     },
     // {
@@ -173,18 +173,28 @@ const questions: Question[] = [
             {text: "바쁘겠지 뭐~", score: {R: 3}},
             {text: "모르겠음", score: {R: 1, D: 1}},
             {text: "혹시 사고 난 건 아니겠지..?", score: {D: 3}},
-            {text: "아니 내가 뭐 잘못한게 있었나...?", score: {D: 5}},
+            {text: "내가 뭐 잘못한게 있었나..?", score: {D: 5}},
         ]
     },
 
     // E vs C
     {
+        question: "친구가 '나 실패한 인생인가 봐...'라고 한다",
+        options: [
+            {text: "“헐 뭔 소리야?! 넌 존재 자체가 선물임”", score: {E: 5}},
+            {text: "“일단 뭐 먹자. 배고프면 인생 다 구려 보여”", score: {E: 3}},
+            {text: "“음... 뭐라 하지?” 머리속 오류남", score: {E: 1, C: 1}},
+            {text: "“구체적으로 어떤 실패를 말하는 거지?” 분석 시작", score: {C: 3}},
+            {text: "“그건 니 생각에 실패고, 기준을 바꾸면 해결임” (멘토톤)", score: {C: 5}},
+        ]
+    },
+    {
         question: "단톡방에 내가 보낸 말에 아무런 반응도 없다",
         options: [
             {text: "‘나… 뭔가 잘못했나…?’ 계속 생각함", score: {E: 5}},
             {text: "‘다들 바쁜가…’ 살짝 서운함", score: {E: 3}},
-            {text: "‘뭐 어쩌라고’ + ‘내가 민감한가?’ 사이에서 계속 갈등", score: {E: 1, C: 1}},
-            {text: "‘이건 굳이 반응할 만한 주제가 아니었음’ 결론 내림", score: {C: 3}},
+            {text: "‘뭐 어쩔’ + ‘내가 민감한가?’ 계속 갈등", score: {E: 1, C: 1}},
+            {text: "‘이건 반응할 만한 주제가 아니었군’ 결론 내림", score: {C: 3}},
             {text: "“사회적 알고리즘상 이 타이밍엔 답장이 없음” 이라며 분석함", score: {C: 5}},
         ]
     },
@@ -199,23 +209,13 @@ const questions: Question[] = [
     //     ]
     // },
     {
-        question: "친구가 '나 진짜 실패한 인생인가 봐...'라고 한다",
-        options: [
-            {text: "“헐 뭔 소리야?! 넌 존재 자체가 선물임” (감성 풀충)", score: {E: 5}},
-            {text: "“일단 뭐 먹자. 배고프면 인생 다 구려 보여”", score: {E: 3}},
-            {text: "“음... 뭐라 하지?” 머리속 오류남", score: {E: 1, C: 1}},
-            {text: "“구체적으로 어떤 실패를 말하는 거지?” 분석 시작", score: {C: 3}},
-            {text: "“그건 니가 생각한 지표에서의 실패고, 기준을 바꾸면 해결임” (멘토톤)", score: {C: 5}},
-        ]
-    },
-    {
         question: "팀 프로젝트 발표에서 실수함",
         options: [
             {text: "‘다 나 때문이야… 팀원들 미안해ㅠㅠ’", score: {E: 5}},
             {text: "혼자서 계속 자책하고 자꾸 생각남", score: {E: 3}},
             {text: "살짝 민망하지만 애써 넘김", score: {E: 1, C: 1}},
             {text: "어떤 파트에서 어떤 실수였는지 분석함", score: {C: 3}},
-            {text: "실수를 정리해서 다음을 대비함 (회복 빠름)", score: {C: 5}},
+            {text: "실수를 정리해서 바로 다음을 대비함 (회복력 갑)", score: {C: 5}},
         ]
     },
     {
@@ -225,7 +225,7 @@ const questions: Question[] = [
             {text: "“이런 거 챙겨주는 거 진짜 감동이야…”", score: {E: 3}},
             {text: "“오… 고마움” 리액션 어떻게할지 고민 중", score: {E: 1, C: 1}},
             {text: "기프티콘 금액 확인 or 주고받은 횟수 비교", score: {C: 3}},
-            {text: "“기프티콘 = 우정 유지 비용 1단계”라고 분석함", score: {C: 5}},
+            {text: "“기프티콘 = 우정 유지 비용” 이라고 분석", score: {C: 5}},
         ]
     },
     // {
@@ -239,13 +239,13 @@ const questions: Question[] = [
     //     ]
     // },
     {
-        question: "SNS에서 친구가 의미심장한 감정적인 글을 올렸다",
+        question: "SNS에서 친구가 의미심장한 감성글을 올렸다",
         options: [
-            {text: "‘헉 무슨 일이지…?’ 벌써 걱정됨", score: {E: 5}},
+            {text: "‘헉 무슨 일이지…?’ 너무 걱정됨", score: {E: 5}},
             {text: "DM 보낼까 말까 10분 고민함", score: {E: 3}},
-            {text: "궁금하긴 한데… 뭐 어쩌지", score: {E: 1, C: 1}},
-            {text: "관찰만 함. 뇌속에서 원인 추정 시작", score: {C: 3}},
-            {text: "“감정적 포스팅은 관심 유도 패턴일 확률 높음” 판단 완료", score: {C: 5}},
+            {text: "궁금하긴 한데… 뭐 그냥 넘긴다", score: {E: 1, C: 1}},
+            {text: "뇌속에서 원인 추정하며 관찰 시작함", score: {C: 3}},
+            {text: "“감성글은 관심 유도 패턴일 확률 높음” 판단 완료", score: {C: 5}},
         ]
     },
     // {
@@ -273,11 +273,11 @@ const questions: Question[] = [
     {
         question: "하루 일정을 어떻게 보내나요?",
         options: [
-            {text: "To-Do리스트, 핸드폰 달력, 메모 습관화", score: {S: 5}},
+            {text: "To-Do, 핸드폰 달력, 메모 습관화", score: {S: 5}},
             {text: "어느 정도 플랜은 있음", score: {S: 3}},
             {text: "아침에 기분 보고 결정함", score: {S: 1, A: 1}},
             {text: "그때그때 느낌 따라 움직임", score: {A: 3}},
-            {text: "계획 세우는 순간 하기 싫어지는편", score: {A: 5}},
+            {text: "계획 세우는 순간 하기 싫어짐", score: {A: 5}},
         ]
     },
     // {
@@ -303,11 +303,11 @@ const questions: Question[] = [
     {
         question: "혼자 카페에서 공부하려고 왔는데 자리 없을 때",
         options: [
-            {text: "플랜 B 카페 3곳 이미 조사 완료", score: {S: 5}},
+            {text: "플랜 B 카페 2곳 이미 조사 완료", score: {S: 5}},
             {text: "근처 조용한 데로 이동", score: {S: 3}},
-            {text: "어쩌지? 고민만 계속함", score: {S: 1, A: 1}},
+            {text: "흠.. 자리 날 때까지 기다림", score: {S: 1, A: 1}},
             {text: "다른 데 갈까 하다 그냥 집으로 돌아옴", score: {A: 3}},
-            {text: "오늘은 아닌가보다~ 공부 포기", score: {A: 5}},
+            {text: "오늘은 아닌가보다~ 빠른 포기", score: {A: 5}},
         ]
     },
     {
@@ -315,9 +315,9 @@ const questions: Question[] = [
         options: [
             {text: "전날 미리 정해놨음", score: {S: 5}},
             {text: "아침에 검색해서 찾아놨음", score: {S: 3}},
-            {text: "간판 찾아보면서 고민함", score: {S: 1, A: 1}},
-            {text: "줄 제일 짧은곳으로 감", score: {A: 3}},
-            {text: "그냥 지인이 가자는 대로 감", score: {A: 5}},
+            {text: "간판 훑어보면서 고민함", score: {S: 1, A: 1}},
+            {text: "웨이팅 제일 짧은곳으로 감", score: {A: 3}},
+            {text: "그냥 아무 데나 끌리는 대로 감", score: {A: 5}},
         ]
     },
     // {
@@ -341,13 +341,13 @@ const questions: Question[] = [
     //     ]
     // },
     {
-        question: "과제 제출 마감시간 23:59 까지",
+        question: "과제 제출 마감시간 23:59!!",
         options: [
             {text: "21시 전에는 무조건 제출", score: {S: 5}},
             {text: "마감 1~2시간 전에 여유롭게", score: {S: 3}},
             {text: "적당히 30분 ~ 1시간 사이에 제출", score: {S: 1, A: 1}},
             {text: "10분 정도 남겨놓고 간신히 올림", score: {A: 3}},
-            {text: "신의 손가락이 나를 도와줌, 기적처럼 세잎", score: {A: 5}},
+            {text: "신이 나를 도와줌, 1분 남기고 세잎", score: {A: 5}},
         ]
     },
     // {
